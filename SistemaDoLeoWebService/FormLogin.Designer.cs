fe8ab8537348eeb96dd9a65c9daff4880c0373b1
Leonardo Fanck <leonardofanck@gmail.com>
2023-08-11 20:46:28 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDoLeoWebService/FormLogin.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SistemaDoLeoWebService
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            TxtID = new TextBox();
+            TxtSenha = new TextBox();
+            BtnAcessar = new Button();
+            BtnFinalizar = new Button();
+            LblNomeOperador = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(89, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(477, 86);
+            label1.TabIndex = 0;
+            label1.Text = "Sistema Do Leo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(119, 170);
+            label2.Name = "label2";
+            label2.Size = new Size(133, 46);
+            label2.TabIndex = 1;
+            label2.Text = "Usuário";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(119, 226);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 46);
+            label3.TabIndex = 2;
+            label3.Text = "Senha";
+            // 
+            // TxtID
+            // 
+            TxtID.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            TxtID.Location = new Point(258, 173);
+            TxtID.MaxLength = 4;
+            TxtID.Name = "TxtID";
+            TxtID.Size = new Size(92, 43);
+            TxtID.TabIndex = 3;
+            TxtID.TextChanged += TxtID_TextChanged;
+            TxtID.KeyPress += TxtID_KeyPress;
+            TxtID.Leave += TxtID_Leave;
+            // 
+            // TxtSenha
+            // 
+            TxtSenha.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            TxtSenha.Location = new Point(258, 229);
+            TxtSenha.MaxLength = 4;
+            TxtSenha.Name = "TxtSenha";
+            TxtSenha.PasswordChar = '*';
+            TxtSenha.Size = new Size(92, 43);
+            TxtSenha.TabIndex = 4;
+            TxtSenha.TextChanged += TxtSenha_TextChanged;
+            TxtSenha.KeyPress += TxtSenha_KeyPress;
+            // 
+            // BtnAcessar
+            // 
+            BtnAcessar.Location = new Point(408, 170);
+            BtnAcessar.Name = "BtnAcessar";
+            BtnAcessar.Size = new Size(131, 51);
+            BtnAcessar.TabIndex = 5;
+            BtnAcessar.Text = "Acessar";
+            BtnAcessar.UseVisualStyleBackColor = true;
+            BtnAcessar.Click += BtnAcessar_Click;
+            // 
+            // BtnFinalizar
+            // 
+            BtnFinalizar.Location = new Point(408, 232);
+            BtnFinalizar.Name = "BtnFinalizar";
+            BtnFinalizar.Size = new Size(131, 51);
+            BtnFinalizar.TabIndex = 6;
+            BtnFinalizar.Text = "Finalizar";
+            BtnFinalizar.UseVisualStyleBackColor = true;
+            BtnFinalizar.Click += BtnFinalizar_Click;
+            // 
+            // LblNomeOperador
+            // 
+            LblNomeOperador.AutoSize = true;
+            LblNomeOperador.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            LblNomeOperador.Location = new Point(239, 324);
+            LblNomeOperador.Name = "LblNomeOperador";
+            LblNomeOperador.Size = new Size(0, 32);
+            LblNomeOperador.TabIndex = 7;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(640, 410);
+            ControlBox = false;
+            Controls.Add(LblNomeOperador);
+            Controls.Add(BtnFinalizar);
+            Controls.Add(BtnAcessar);
+            Controls.Add(TxtSenha);
+            Controls.Add(TxtID);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "FormLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sistema do Leo - Login";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox TxtID;
+        private TextBox TxtSenha;
+        private Button BtnAcessar;
+        private Button BtnFinalizar;
+        private Label LblNomeOperador;
+    }
+}
---
+++ SistemaDoLeoWebService/FormLogin.cs
@@ -0,0 +1,147 @@
+using Microsoft.VisualBasic;
+using System.Data;
+
+namespace SistemaDoLeoWebService
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8) // SOMENTE NÚMERO E BACKSPACE
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == 13) // ENTER
+            {
+                TxtSenha.Focus();
+            }
+        }
+
+        private void TxtID_Leave(object sender, EventArgs e)
+        {
+            // VAI CHAMAR A FUNÇÃO QUE CONECTA COM O WEB SERVICE
+            // E CHAMAR O VALIDA OPERADOR
+
+            int ID;
+            var WebReference = new ServiceReference1.Service1Client();
+
+            if (TxtID.Text != "")
+            {
+                ID = int.Parse(TxtID.Text);
+                int resultado = WebReference.VerificaOperadorAsync(ID).Result;
+
+                if (resultado.Equals(0))
+                {
+                    LblNomeOperador.Text = WebReference.getNomeOperadorAsync(ID).Result;
+                }
+                else if (resultado.Equals(1))
+                {
+                    MessageBox.Show("Operador " + ID + " Inativo");
+                    TxtID.Text = "";
+                    TxtID.Focus();
+                }
+                else if (resultado.Equals(2))
+                {
+                    MessageBox.Show("Operador não Cadastrado");
+                    TxtID.Text = "";
+                    TxtID.Focus();
+                }
+            }
+        }
+
+        private void BtnFinalizar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void TxtID_TextChanged(object sender, EventArgs e)
+        {
+            limpaCampos();
+        }
+
+        private void TxtSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtSenha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) // ENTER
+            {
+                BtnAcessar.Focus();
+            }
+        }
+
+        private void limpaCampos()
+        {
+            TxtSenha.Text = string.Empty;
+            LblNomeOperador.Text = string.Empty;
+        }
+
+        private void BtnAcessar_Click(object sender, EventArgs e)
+        {
+            // DECLARAÇÕES
+            int ID;
+            int Senha;
+            var WebReference = new ServiceReference1.Service1Client();
+
+            // VERIFICA SE OS CAMPOS ESTÃO PREENCHIDOS
+            if (TxtID.Text == "")
+            {
+                MessageBox.Show("Informe um operador!");
+                TxtID.Focus();
+            }
+            else if (TxtSenha.Text == "")
+            {
+                MessageBox.Show("Informe a senha!");
+                TxtSenha.Focus();
+            }
+            else
+            {
+                // PEGA OS DADOS DOS TXT'S
+                ID = int.Parse(TxtID.Text);
+                Senha = int.Parse(TxtSenha.Text);
+
+                // CHAMA A FUNÇÃO DO WEB SERVICE
+                int resultado = WebReference.VerificaOperadorAsync(ID).Result;
+
+                // VERIFICA OS RESULTADOS
+                if (resultado.Equals(0))
+                {
+                    LblNomeOperador.Text = WebReference.getNomeOperadorAsync(ID).Result;
+                }
+                else if (resultado.Equals(1))
+                {
+                    MessageBox.Show("Operador " + ID + " Inativo");
+                    TxtID.Text = "";
+                    TxtID.Focus();
+                }
+                else if (resultado.Equals(2))
+                {
+                    MessageBox.Show("Operador não Cadastrado");
+                    TxtID.Text = "";
+                    TxtID.Focus();
+                }
+                else if (resultado.Equals(-1))
+                {
+                    MessageBox.Show("Ocorreu um erro ao tentar verificar o Operador");
+                    TxtID.Text = "";
+                    TxtID.Focus();
+                }
+            }
+        }
+    }
+
+}
